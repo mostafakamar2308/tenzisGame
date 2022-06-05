ab11586ea1575ac4816d58cf254d1f4e11cd2b55
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2022-06-05 07:37:39 +0200

    Finished the project finally :'D

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+
+function Cube(props) {
+  return (
+    <div
+      className={`cube ${props.class ? "freeze" : ""}`}
+      onClick={props.handleClick}
+      id={props.id}
+    >
+      {props.number}
+    </div>
+  );
+}
+
+function Game() {
+  const [won, setWon] = React.useState(false);
+  const [gameCubesState, setGameCubesState] = React.useState(
+    [...Array(10)].map((ele) => {
+      return { freeze: false, number: Math.floor(Math.random() * 10 + 1) };
+    })
+  );
+  useEffect(() => {
+    let gameSet = new Set(gameCubesState.map((ele) => ele.number));
+    gameSet.size === 1 ? setWon(true) : setWon(false);
+  }, [gameCubesState]);
+
+  if (won === true) {
+    return (
+      <div>
+        <div className="Winning">You Win </div>
+        <button
+          onClick={() => {
+            setWon(false);
+            setGameCubesState(
+              [...Array(10)].map((ele) => {
+                return {
+                  freeze: false,
+                  number: Math.floor(Math.random() * 10 + 1),
+                };
+              })
+            );
+          }}
+          className="replay"
+        >
+          {" "}
+          Wanna Replay?
+        </button>
+      </div>
+    );
+  }
+
+  function freezeCube(e) {
+    let targetCubeIndex = e.target.id;
+    setGameCubesState((oldGameCubesState) => {
+      return oldGameCubesState.map((ele, index) => {
+        return Number(targetCubeIndex) === index
+          ? { ...ele, freeze: !ele.freeze }
+          : { ...ele };
+      });
+    });
+  }
+
+  function rollCubes() {
+    setGameCubesState((oldGameCubesState) => {
+      let newMap = oldGameCubesState.map((ele) =>
+        ele.freeze
+          ? { ...ele }
+          : { ...ele, number: Math.floor(Math.random() * 10 + 1) }
+      );
+      return newMap;
+    });
+  }
+
+  return (
+    <div id="game">
+      <div className="details">
+        <h1>Tenzis</h1>
+        <p>
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </div>
+      <div className="game-container">
+        {gameCubesState.map((ele, index) => (
+          <Cube
+            number={ele.number}
+            id={index}
+            key={`cube-${index}`}
+            class={ele.freeze}
+            handleClick={freezeCube}
+          ></Cube>
+        ))}
+      </div>
+      <button className="roll" onClick={rollCubes}>
+        Roll
+      </button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game />
     </div>
   );
 }
